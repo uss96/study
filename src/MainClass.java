51f904ed0366fa8011227e4def72a8508dc61a74
uiseok <uiseoks@naver.com>
2022-08-01 23:17:44 +0900

    인프런 - 자바 프로그래밍 입문 강좌 (1~4강)

---
+++ src/MainClass.java
@@ -0,0 +1,18 @@
+public class MainClass {
+
+    public static void main(String[] args) { // 프로그램이 시작될 때 가장 먼저 시작되는 method (main)
+
+        int i = 10;
+        int j;
+        j = 100;
+        System.out.println("i + j = " + (i+j));
+
+        i = 100;
+        System.out.println("i = " + i);
+
+        i = 300;
+        System.out.println("i = " + i);
+
+
+    }
+}
