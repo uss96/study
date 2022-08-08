0672d2509d1b27e92615aafda5b5c13cae3a976a
uiseok <uiseoks@naver.com>
2022-08-08 23:42:13 +0900

    인프런 - 자바 프로그래밍 입문 강좌 (8~10강)

---
+++ src/ex09.java
@@ -0,0 +1,14 @@
+public class ex09 {
+
+    public class Bicycle {
+        public String color;
+        public int price;
+
+        public Bicycle () {
+            System.out.println(" --info() --");
+            System.out.println("color : " + color);
+            System.out.println("price : " + price);
+
+        }
+    }
+}
---
+++ src/ex10.java
@@ -0,0 +1,26 @@
+public class ex10 {
+
+    public static void main(String arg[] args) {
+
+
+        Grandeur myCar1 = new Grandeur();
+        myCar1.color = "red";
+        myCar1.gear = "auto";
+        myCar1.price = 30000000;
+
+        myCar1.run();
+        myCar1.stop();
+        myCar1.info();
+
+        System.out.println();
+
+        Grandeur myCar2 = new Grandeur();
+        myCar2.color = blue;
+        myCar2.gear = manual;
+        myCar2.price = 25000000;
+
+        myCar2.run();
+        myCar2.stop();
+        myCar2.info();
+    }
+}
